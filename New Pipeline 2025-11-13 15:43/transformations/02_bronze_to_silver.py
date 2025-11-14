60dad44951cce10fcb191de216ab87caeec64243
CarrotHead1-1 <kieron.escott@icloud.com>
2025-11-14 17:54:35 +0000

    added star schema in the gold layer

--- New Pipeline 2025-11-13 15:43/transformations/02_bronze_to_silver.py
+++ New Pipeline 2025-11-13 15:43/transformations/02_bronze_to_silver.py
@@ -12,16 +12,42 @@ from utilities import team_names
 
 @dlt.expect("valid_match_id", "MatchID IS NOT NULL")
 
+#drop null matches
+@dlt.expect_or_drop("valid_home_team", "HomeTeam IS NOT NULL")
+@dlt.expect_or_drop("valid_away_team", "AwayTeam IS NOT NULL")
+@dlt.expect_or_drop("valid_season", "Season IS NOT NULL")
+@dlt.expect_or_drop("valid_round", "Round IS NOT NULL")
+@dlt.expect_or_drop("valid_home_team_score", "HomeTeamScore IS NOT NULL")
+@dlt.expect_or_drop("valid_away_team_score", "AwayTeamScore IS NOT NULL")
+
+#validate teams and score 
+@dlt.expect("valid_scores", "HomeScore >= 0 AND AwayScore >= 0")
+@dlt.expect_or_drop("different_teams", "HomeTeam != AwayTeam")
+
 def match_results():
     df = dlt.readStream("rubgy_data_dev.rugby_bronze.match_results_raw")
 
     #normalise team names
     df = team_names.normalise_team_names(df)
+
+    #drop duplicate matches
+    df = df.drop_duplicates()
+
+    #create results column and points difference 
+    df = df.withColumn("Result", 
+        when(col("HomeScore") > col("AwayScore"), "HomeWin")
+        .when(col("HomeScore") < col("AwayScore"), "AwayWin")
+        .otherwise("Draw")
+        )
+
+    df = df.withColumn("HomePointsDifference",
+        (col("HomeScore") - col("AwayScore")).cast("int")
+        )
+
+    df = df.withColumn("AwayPointsDifference",
+        (col("AwayScore") - col("HomeScore")).cast("int")
+        )
+    
+    #enforce schema?
+    
     return df
-    
-    
-
-
-
-
-    #validate matach results 
