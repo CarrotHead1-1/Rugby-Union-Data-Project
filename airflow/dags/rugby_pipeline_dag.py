78b215825ff70777e9bbfb386c1619d608abc9bc
up2109872 <up2109872@myport.ac.uk>
2025-12-21 16:28:32 +0000

    setting up airflow

---
+++ airflow/dags/rugby_pipeline_dag.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="rugby_pipeline",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["rugby", "databricks"],
+) as dag:
+
+    start = BashOperator(
+        task_id="start_pipeline",
+        bash_command="echo 'Starting Rugby Data Pipeline'",
+    )
+
+    end = BashOperator(
+        task_id="end_pipeline",
+        bash_command="echo 'Pipeline Completed Successfully'",
+    )
+
+    start >> end
