83aee56df95003610b88b2ced0bcfd9f3c93cfc4
up2109872 <up2109872@myport.ac.uk>
2025-12-24 17:24:07 +0000

    updated dag file to run databricks job

--- airflow/dags/rugby_pipeline.py
+++ airflow/dags/rugby_pipeline.py
@@ -12,15 +12,22 @@ with DAG (
 
 ) as dag:
     
-    start = DatabricksRunNowOperator(
-        task_id = "start_pipeline",
-        bash_command = "echo 'Starting Rugby Data Pipeline'",
+    trigger_dlt = DatabricksRunNowOperator(
+        task_id = "trigger_dlt_pipeline",
+        databricks_conn_id = "databricks_default",
+        job_id = 640017615260074 #databricks job id
     )
+    
+    #local setup 
+    # start = BashOperator(
+    #     task_id = "start_pipeline",
+    #     bash_command = "echo 'Starting Rugby Data Pipeline'",
+    # )
 
-    end = DatabricksRunNowOperator(
-        task_id = "end_pipeline",
-        bash_command = "echo 'Pipeline Completed Successfully'",
-    )
+    # end = BashOperator(
+    #     task_id = "end_pipeline",
+    #     bash_command = "echo 'Pipeline Completed Successfully'",
+    # )
 
-    start >> end
+    # start >> end
 
